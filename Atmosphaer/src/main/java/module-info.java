a72c0fe9e815ea125cc7ddb3cdf5db3579a27d19
PRMulio <pablorodriguezmulio@gmail.com>
2024-05-01 15:22:45 +0200

    delete netbeans project

--- Atmosphaer/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.practicas.zephyrosfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.practicas.atmosphaer to javafx.fxml;
-    exports com.practicas.atmosphaer;
-    requires org.json;
-}
